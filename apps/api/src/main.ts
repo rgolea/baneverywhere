d0e5dffdae5f42ac0588b6debeac4621699328b4
Rares Golea <rgolea@users.noreply.github.com>
2021-10-18 20:19:43 +0200

    Fixing nx serve on api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,8 +13,9 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  await app.listen(port, async () => {
+    app.getUrl().then((url) => Logger.log(`URL: ${url}`))
+    // Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
 }
 
