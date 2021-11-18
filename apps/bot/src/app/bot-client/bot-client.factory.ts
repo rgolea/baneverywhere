bbd55bc95ebf29417f41ce71223448bd3eb73771
Rares Golea <rgolea@users.noreply.github.com>
2021-11-18 22:00:50 +0100

    Fixing Dockerfiles

--- apps/bot/src/app/bot-client/bot-client.factory.ts
+++ apps/bot/src/app/bot-client/bot-client.factory.ts
@@ -14,8 +14,7 @@ export async function botClientFactory(
     identity: {
       username: opts.username,
       password: `oauth:${opts.accessToken}`,
-    },
-    channels: ['rgolea', 'baneverywhere']
+    }
   });
 
   await client.connect().catch(err => console.error(err));
