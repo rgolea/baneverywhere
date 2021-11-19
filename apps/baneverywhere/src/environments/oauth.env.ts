55fd3e9019cf64dff6d948a4c2d7865afeb8c045
Rares Golea <rgolea@users.noreply.github.com>
2021-11-19 01:25:39 +0100

    Prepare for production deploy

--- apps/baneverywhere/src/environments/oauth.env.ts
+++ apps/baneverywhere/src/environments/oauth.env.ts
@@ -3,6 +3,6 @@ import { WebOption } from '@byteowls/capacitor-oauth2';
 export const OAUTH_ENV: WebOption = {
   redirectUrl: `${window.location.origin}/auth`,
   scope: 'user_read user:read:follows',
-  appId: 'b361z2jtreid3u1demvx2v6xrvwfqr',
+  appId: 'w9y5kumto3xoyb3oomepaqz8si44pq',
   authorizationBaseUrl: 'https://id.twitch.tv/oauth2/authorize',
 };
