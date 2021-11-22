95ffbdef6156ad9064b4d6679ca885d6bdf055d6
Rares Golea <rgolea@users.noreply.github.com>
2021-11-23 00:45:01 +0100

    Debugging bot

--- apps/baneverywhere/src/environments/oauth.env.ts
+++ apps/baneverywhere/src/environments/oauth.env.ts
@@ -4,6 +4,5 @@ export const OAUTH_ENV: WebOption = {
   redirectUrl: `${window.location.origin}/auth`,
   scope: 'user_read user:read:follows',
   appId: 'w9y5kumto3xoyb3oomepaqz8si44pq',
-  authorizationBaseUrl: 'https://id.twitch.tv/oauth2/authorize',
-  responseType: 'access_token'
+  authorizationBaseUrl: 'https://id.twitch.tv/oauth2/authorize'
 };
