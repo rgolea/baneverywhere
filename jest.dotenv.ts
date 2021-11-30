b16bf7b05a4513c7ada56157b355b373793014c9
Rares Golea <rgolea@users.noreply.github.com>
2021-11-30 06:27:10 +0100

    Removing bot-handler and running with bot and online-checker

--- jest.dotenv.ts
+++ jest.dotenv.ts
@@ -5,4 +5,6 @@ config({
   path: `.env.test`,
 });
 
-execSync(`DATABASE_URL=${process.env.DATABASE_URL} npx prisma migrate deploy`);
+if(!process.env.CI){
+  execSync(`DATABASE_URL=${process.env.DATABASE_URL} npx prisma migrate deploy`);
+}
