f13d6089e3fc20cb144353a9bf5d47e3279bbb68
Rares Golea <rgolea@users.noreply.github.com>
2021-11-30 23:29:57 +0100

    Solving access token

--- apps/online-checker/src/app/app.module.ts
+++ apps/online-checker/src/app/app.module.ts
@@ -3,27 +3,15 @@ import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { BotDatabaseModule } from "@baneverywhere/db";
 import { ConfigModule, ConfigService } from "@nestjs/config";
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ClientProxyFactory, ClientsModule, Transport } from '@nestjs/microservices';
 import { BOT_CONNECTION } from "@baneverywhere/namespaces";
 import { TwitchClientModule } from "@baneverywhere/twitch-client";
 @Module({
   imports: [
-    ConfigModule,
+    ConfigModule.forRoot(),
     ScheduleModule.forRoot(),
     BotDatabaseModule,
-    ClientsModule.registerAsync([
-      {
-        imports: [ConfigModule],
-        useFactory: async (config: ConfigService) => ({
-          transport: Transport.REDIS,
-          options: {
-            url: `redis://${config.get('REDIS_HOST', 'localhost')}:${config.get('REDIS_PORT', 6379)}`,
-          }
-        }),
-        inject: [ConfigService],
-        name: BOT_CONNECTION,
-      },
-    ]),
+    ClientsModule.register([]),
     TwitchClientModule.forRootAsync({
       useFactory: (config) => ({
         clientID: config.get('TWITCH_CLIENT_ID'),
@@ -33,6 +21,18 @@ import { TwitchClientModule } from "@baneverywhere/twitch-client";
       imports: [ConfigModule]
     })
   ],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: BOT_CONNECTION,
+    useFactory: (config: ConfigService) => {
+      return ClientProxyFactory.create({
+        transport: Transport.REDIS,
+        options: {
+          url: `redis://${config.get('REDIS_HOST', 'localhost')}:${config.get('REDIS_PORT', 6379)}`,
+        }
+      })
+    },
+    inject: [ConfigService]
+  }],
 })
-export class AppModule {}
+export class AppModule {
+}
